9e5cad908684fb771cf1712058f324132cd20357
Paul Jolly <paul@myitcv.io>
2021-02-26 16:37:42 +0000

    projects: add PWG as the first unity project (#19)

---
+++ _scripts/runUnity.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+# Move to the root of the repo that contains this script
+cd "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../"
+commit=$(git rev-parse HEAD)
+export GOBIN=$PWD/.bin
+if [ "${GITHUB_REF:-}" == "refs/heads/main" ]
+then
+	go install github.com/cue-sh/unity/cmd/unity@$commit
+else
+	go install github.com/cue-sh/unity/cmd/unity
+fi
+
+exec $GOBIN/unity test --corpus
